73d7f70b4efa49eb588278f91b5ebf026f2f5c22
Gerges Ghaly <82944591+GergesGhaly@users.noreply.github.com>
2024-10-22 12:25:24 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import {
 } from "react-router-dom";
 import App from "./App";
 import LoginPage from "./LoginPage";
-import UserMenu from "./UserMenu";
 import ChatPage from "./ChatPage";
 import { auth } from "./Firebase"; // تأكد من استيراد المصادقة من Firebase
 import "./index.css";
